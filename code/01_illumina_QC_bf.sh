45c00e036730c7a4b615f75f77b59cb5d2201029
Lei Zhao <leizhao@rackham2.uppmax.uu.se>
2023-03-31 10:36:04 +0200

    add files

---
+++ code/01_illumina_QC_bf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2023-2-8 
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 00:30:00
+#SBATCH -J Illumina_QC_bf
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user lei.zhao.1300@student.uu.se
+
+module load bioinfo-tools FastQC
+
+fastqc -o /home/leizhao/Genome_Analysis/results/FastQC  /home/leizhao/Genome_Analysis/data/raw_data/illumina_data/SRR6058604_scaffold_06.*P.fastq.gz
+
